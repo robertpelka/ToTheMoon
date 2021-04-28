9f5c4d37942d688a7da8ded914d2f47fe5977318
Robert Pelka <123455ff@gmail.com>
2021-04-28 21:47:08 +0200

    Add teleportating the ball when it's outside the screen

--- ToTheMoon/GameScene.swift
+++ ToTheMoon/GameScene.swift
@@ -11,6 +11,7 @@ import CoreMotion
 class GameScene: SKScene {
     
     var motionManager: CMMotionManager!
+    let ball = SKShapeNode(circleOfRadius: 20.0)
     
     override func didMove(to view: SKView) {
         motionManager = CMMotionManager()
@@ -24,9 +25,13 @@ class GameScene: SKScene {
         
         physicsWorld.contactDelegate = self
         
-        let ball = SKShapeNode(circleOfRadius: 20.0)
+        spawnBall()
+        spawnPlatform()
+    }
+    
+    func spawnBall() {
         ball.fillColor = UIColor.yellow
-        ball.position = CGPoint(x: frame.midX, y: frame.minY + ball.frame.height * 4)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
         ball.name = "Ball"
         ball.physicsBody = SKPhysicsBody(circleOfRadius: 20.0)
         ball.physicsBody?.affectedByGravity = true
@@ -34,7 +39,9 @@ class GameScene: SKScene {
         ball.physicsBody?.contactTestBitMask = PhysicsCategories.platformCategory
         ball.physicsBody?.collisionBitMask = PhysicsCategories.none
         addChild(ball)
-        
+    }
+    
+    func spawnPlatform() {
         let platform = SKShapeNode(rectOf: CGSize(width: 120, height: 20))
         platform.position = CGPoint(x: frame.midX, y: frame.minY + 30)
         platform.fillColor = UIColor.red
@@ -46,9 +53,58 @@ class GameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
+        checkPhoneTilt()
+        checkBallPosition()
+        checkBallVelocity()
+    }
+    
+    func checkPhoneTilt() {
+        let defaultA = 9.8
         if let accelerometerData = motionManager.accelerometerData {
-            physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.x * 30, dy: -9.8)
+            var xAcceleration = accelerometerData.acceleration.x * 50
+            if xAcceleration > defaultA {
+                xAcceleration = defaultA
+            }
+            else if xAcceleration < -defaultA {
+                xAcceleration = -defaultA
+            }
+            physicsWorld.gravity = CGVector(dx: xAcceleration, dy: -defaultA)
         }
+    }
+    
+    func checkBallPosition() {
+        if let ballWidth = ball.path?.boundingBox.size.width {
+            if ball.position.x-ballWidth >= frame.size.width || ball.position.x+ballWidth <= 0 {
+                fixBallPosition()
+            }
+        }
+    }
+    
+    func checkBallVelocity() {
+        if let ballVelocity = ball.physicsBody?.velocity.dx {
+            if ballVelocity > 1000 {
+                ball.physicsBody?.velocity.dx = 1000
+            }
+            else if ballVelocity < -1000 {
+                ball.physicsBody?.velocity.dx = -1000
+            }
+        }
+    }
+    
+    func fixBallPosition() {
+        if let ballWidth = ball.path?.boundingBox.size.width {
+            if ball.position.x >= frame.size.width {
+                ball.position.x = 0 - ballWidth/2+1
+            }
+            else {
+                ball.position.x = frame.size.width + ballWidth/2-1
+            }
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        ball.removeFromParent()
+        spawnBall()
     }
 
 }
@@ -58,11 +114,9 @@ extension GameScene: SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
         let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
         if contactMask == PhysicsCategories.ballCategory | PhysicsCategories.platformCategory {
-            if let ball = contact.bodyA.node?.name == "Ball" ? contact.bodyA.node as? SKShapeNode : contact.bodyB.node as? SKShapeNode {
-                if let ballVelocity = ball.physicsBody?.velocity.dy {
-                    if ballVelocity < 0 {
-                        ball.physicsBody?.velocity.dy = 1000
-                    }
+            if let ballVelocity = ball.physicsBody?.velocity.dy {
+                if ballVelocity < 0 {
+                    ball.physicsBody?.velocity.dy = 1000
                 }
             }
         }
